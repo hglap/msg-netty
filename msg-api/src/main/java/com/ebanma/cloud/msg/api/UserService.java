3be279c071873ca134b15dd8b9c2041ab173a9d0
黄贵龙 <hgl01622487@alibaba-inc.com>
2023-02-15 10:36:53 +0800

    脚手架生成代码

---
+++ msg-api/src/main/java/com/ebanma/cloud/msg/api/UserService.java
@@ -0,0 +1,14 @@
+package com.ebanma.cloud.msg.api;
+
+
+import com.ebanma.cloud.msg.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
---
+++ start/src/test/java/com/ebanma/cloud/msg/MsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanma.cloud.msg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
